d98d2c8b0cc69f1a0ce1605f31c232a0e481e5b1
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2014-01-17 11:45:29 -0800

    moving files

---
+++ src/WebJobsVs/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace LigerShark.WebJobsVs
+{
+    static class GuidList
+    {
+        public const string guidWebJobsVsPkgString = "e42a8517-258b-48d1-a92e-aba474d8def8";
+        public const string guidWebJobsVsCmdSetString = "c18c51ba-3e45-4ff2-acae-e8b665020949";
+
+        public static readonly Guid guidWebJobsVsCmdSet = new Guid(guidWebJobsVsCmdSetString);
+    };
+}
---
+++ src/WebJobsVs/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace LigerShark.WebJobsVs
+{
+    static class PkgCmdIDList
+    {
+        public const uint WebJobsAddProject =        0x100;
+
+
+    };
+}
