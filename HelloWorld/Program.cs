dff83771245071102bfeb9afff7763dc01c4ec1b
rustd <pranavra@PRANAVRA0808.redmond.corp.microsoft.com>
2014-01-17 10:52:49 -0800

    hello world sample

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.WindowsAzure.Jobs;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        // Please set the following connectionstring values in app.config
+        // AzureJobsRuntime and AzureJobsData
+        static void Main()
+        {
+            JobHost host = new JobHost();
+            host.RunAndBlock();
+        }
+
+        public static void ProcessQueueMessage([QueueInput] string inputText, [BlobOutput("containername/blobname")]TextWriter writer)
+        {
+            writer.WriteLine(inputText);
+        }
+    }
+}
