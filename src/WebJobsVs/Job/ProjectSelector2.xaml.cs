1dab3a992f2bde1de1225598c123620005a7da66
Mads Kristensen <post@madskristensen.net>
2014-01-17 17:56:03 -0800

    Hooking up the tooling

---
+++ src/WebJobsVs/Job/ProjectSelector2.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Linq;
+
+namespace LigerShark.WebJobsVs.Dialog
+{
+    public partial class ProjectSelector2 : Window
+    {
+        private IEnumerable<string> _projectNames;
+
+        public ProjectSelector2(IEnumerable<string> projectNames)
+        {
+            InitializeComponent();
+
+            _projectNames = projectNames;
+            
+            if (projectNames.Any())
+            {
+                names.ItemsSource = _projectNames;
+                names.SelectedIndex = 0;
+            }
+            else
+            {
+                names.IsEnabled = false;
+                btnOk.IsEnabled = false;
+            }
+        }
+
+        public string SelectedProjectName { get; set; }
+        public string Schedule { get; set; }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SelectedProjectName = names.SelectedItem.ToString();
+            Schedule = continuous.IsChecked.Value ? "continuous" : "triggered";
+            DialogResult = true;
+        }
+    }
+}
---
+++ src/WebJobsVs/Job/WebJobCreator.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using EnvDTE;
+
+namespace LigerShark.WebJobsVs
+{    
+    class WebJobCreator
+    {
+        public void AddReference(Project currentProject, Project selectedProject)
+        {
+            VSLangProj.VSProject vslangProject = currentProject.Object as VSLangProj.VSProject;
+            vslangProject.References.AddProject(selectedProject);
+        }
+
+        public void CreateFolders(Project currentProject, string schedule)
+        {
+            string dir = Path.GetDirectoryName(currentProject.FullName);
+
+            string appData = Path.Combine(dir, "App_Data");
+            if (!Directory.Exists(appData))
+                Directory.CreateDirectory(appData);
+
+            string jobs = Path.Combine(appData, "jobs");
+            if (!Directory.Exists(jobs))
+                Directory.CreateDirectory(jobs);
+
+            string method = Path.Combine(jobs, schedule);
+            if (!Directory.Exists(method))
+                Directory.CreateDirectory(method);
+
+            string readmeFile = Path.Combine(method, "readme.txt");
+            AddReadMe(readmeFile);
+            currentProject.ProjectItems.AddFromFile(readmeFile);
+        }
+
+        private static void AddReadMe(string destinationFileName)
+        {
+            string dir = Path.GetDirectoryName(typeof(WebJobCreator).Assembly.Location);
+            string readme = Path.Combine(dir, "job", Path.GetFileName(destinationFileName));
+
+            File.Copy(readme, destinationFileName, true);
+        }
+    }
+}
