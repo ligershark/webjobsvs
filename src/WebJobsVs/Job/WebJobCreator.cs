7d151924f92f09c0ade8d40b6a767537137fdb18
Mads Kristensen <post@madskristensen.net>
2014-01-20 21:05:39 -0800

    Support for website projects

--- src/WebJobsVs/Job/WebJobCreator.cs
+++ src/WebJobsVs/Job/WebJobCreator.cs
@@ -2,18 +2,33 @@
 using EnvDTE;
 
 namespace LigerShark.WebJobsVs
-{    
+{
     class WebJobCreator
     {
         public void AddReference(Project currentProject, Project selectedProject)
         {
-            VSLangProj.VSProject vslangProject = currentProject.Object as VSLangProj.VSProject;
-            vslangProject.References.AddProject(selectedProject);
+            if (currentProject.Object is VSLangProj.VSProject)
+            {
+                var project = currentProject.Object as VSLangProj.VSProject;
+                project.References.AddProject(selectedProject);
+            }
+            else if (currentProject.Object is VsWebSite.VSWebSite)
+            {
+                var project = currentProject.Object as VsWebSite.VSWebSite;
+                try
+                {
+                    project.References.AddFromProject(selectedProject);
+                }
+                catch
+                {
+                    // Reference was already added. TODO: don't use try/catch for this.
+                }
+            }
         }
 
         public void CreateFolders(Project currentProject, string schedule, string projectName)
         {
-            string dir = Path.GetDirectoryName(currentProject.FullName);
+            string dir = GetProjectDirectory(currentProject);
             DirectoryInfo info = new DirectoryInfo(dir)
                 .CreateSubdirectory("App_Data\\jobs")
                 .CreateSubdirectory(schedule)
@@ -22,6 +37,14 @@ namespace LigerShark.WebJobsVs
             string readmeFile = Path.Combine(info.FullName, "readme.txt");
             AddReadMe(readmeFile);
             currentProject.ProjectItems.AddFromFile(readmeFile);
+        }
+
+        private static string GetProjectDirectory(Project project)
+        {
+            if (project.Object is VSLangProj.VSProject)
+                return Path.GetDirectoryName(project.FullName);
+
+            return project.Properties.Item("fullPath").Value.ToString();
         }
 
         private static void AddReadMe(string destinationFileName)
